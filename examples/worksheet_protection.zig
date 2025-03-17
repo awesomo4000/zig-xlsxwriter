2c0bc6b3b121ae8e19a9c67b51037e559ea9bb0e
awesomo4000 <alrhodes@gmail.com>
2025-03-17 10:05:00 -0400

    worksheet_protection example

--- examples/worksheet_protection.zig
+++ examples/worksheet_protection.zig
@@ -10,29 +10,88 @@ const std = @import("std");
 const xlsxwriter = @import("xlsxwriter");
 
 pub fn main() !void {
-    const workbook = xlsxwriter.workbook_new("zig-worksheet_protection.xlsx");
-    const worksheet = xlsxwriter.workbook_add_worksheet(workbook, null);
+    const workbook =
+        xlsxwriter.workbook_new(
+            "zig-worksheet_protection.xlsx",
+        );
+    const worksheet =
+        xlsxwriter.workbook_add_worksheet(
+            workbook,
+            null,
+        );
 
-    const unlocked = xlsxwriter.workbook_add_format(workbook);
+    const unlocked =
+        xlsxwriter.workbook_add_format(
+            workbook,
+        );
     _ = xlsxwriter.format_set_unlocked(unlocked);
 
-    const hidden = xlsxwriter.workbook_add_format(workbook);
+    const hidden =
+        xlsxwriter.workbook_add_format(
+            workbook,
+        );
     _ = xlsxwriter.format_set_hidden(hidden);
 
     // Widen the first column to make the text clearer
-    _ = xlsxwriter.worksheet_set_column(worksheet, 0, 0, 40, null);
+    _ = xlsxwriter.worksheet_set_column(
+        worksheet,
+        0,
+        0,
+        40,
+        null,
+    );
 
     // Turn worksheet protection on without a password
-    _ = xlsxwriter.worksheet_protect(worksheet, null, null);
+    _ = xlsxwriter.worksheet_protect(
+        worksheet,
+        null,
+        null,
+    );
 
     // Write a locked, unlocked and hidden cell
-    _ = xlsxwriter.worksheet_write_string(worksheet, 0, 0, "B1 is locked. It cannot be edited.", null);
-    _ = xlsxwriter.worksheet_write_string(worksheet, 1, 0, "B2 is unlocked. It can be edited.", null);
-    _ = xlsxwriter.worksheet_write_string(worksheet, 2, 0, "B3 is hidden. The formula isn't visible.", null);
+    _ = xlsxwriter.worksheet_write_string(
+        worksheet,
+        0,
+        0,
+        "B1 is locked. It cannot be edited.",
+        null,
+    );
+    _ = xlsxwriter.worksheet_write_string(
+        worksheet,
+        1,
+        0,
+        "B2 is unlocked. It can be edited.",
+        null,
+    );
+    _ = xlsxwriter.worksheet_write_string(
+        worksheet,
+        2,
+        0,
+        "B3 is hidden. The formula isn't visible.",
+        null,
+    );
 
-    _ = xlsxwriter.worksheet_write_formula(worksheet, 0, 1, "=1+2", null); // Locked by default
-    _ = xlsxwriter.worksheet_write_formula(worksheet, 1, 1, "=1+2", unlocked);
-    _ = xlsxwriter.worksheet_write_formula(worksheet, 2, 1, "=1+2", hidden);
+    _ = xlsxwriter.worksheet_write_formula(
+        worksheet,
+        0,
+        1,
+        "=1+2",
+        null,
+    ); // Locked by default
+    _ = xlsxwriter.worksheet_write_formula(
+        worksheet,
+        1,
+        1,
+        "=1+2",
+        unlocked,
+    );
+    _ = xlsxwriter.worksheet_write_formula(
+        worksheet,
+        2,
+        1,
+        "=1+2",
+        hidden,
+    );
 
     _ = xlsxwriter.workbook_close(workbook);
 }
